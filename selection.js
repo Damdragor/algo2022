39fecc8ac065f184e53bb34b8a947c59ed4dd646
Kirill <95622151+Damdragor@users.noreply.github.com>
2022-09-25 21:46:36 +0300

    Update selection.js

--- selection.js
+++ selection.js
@@ -1,26 +1,22 @@
 let array = [3, 1, 4, 3, 2, -5, 5, -12, -3, 2];
-console.log(selectionSort(array));
+selectionSort(array);
+console.log(array);
 
-function selectionSort(array) {
-	let result = [];
-	let length = array.length;
-
-	for (let i = 0; i < length; i++) {
-		let min = findSmall(array);
-		result.push(array.splice(min, 1)[0]);
-	}
-	return result;
-}
-
-function findSmall(array) { 
-	let minElem = array[0];
-	let minIndex = 0;
-
-	for (let i = 0; i < array.length; i++) {
-		if (array[i] < minElem) {
-			minElem = array[i];
-			minIndex = i;
-		}
-	}
-	return minIndex;
+function selectionSort(array) { 
+    let n = array.length;
+        
+    for(let i = 0; i < n; i++) {
+        let min = i;
+        for(let j = i; j < n; j++) {
+            if(array[j] < array[min]) {
+                min=j; 
+            }
+         }
+         if (min != i) {
+             let tmp = array[i]; 
+             array[i] = array[min];
+             array[min] = tmp;      
+        }
+    }
+    return array;
 }
